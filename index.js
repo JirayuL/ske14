c39d37c1a27d3a58c60843f5f83e579540a32e28
masty123 <theeruth.b@ku.th>
2020-10-27 09:04:58 +0700

    add epic console.log

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log(ske14)
 console.log('ske14')
 console.log('ske' + 14)
 console.log('skec invasiton')
-console.log('SKES');
+console.log('me like tities milky');
 function SKE14() {
   return <View><Text>SKE14</Text></View>
 }
