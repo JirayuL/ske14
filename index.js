a9ffe23c7242f897120d15e2d8f93729203c51ef
masty123 <theeruth.b@ku.th>
2020-10-27 08:57:55 +0700

    invaded

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { View, Text }, ske14 from 'ske14'
 console.log(ske14)
 console.log('ske14')
 console.log('ske' + 14)
-
+console.log('skec invasiton')
 function SKE14() {
   return <View><Text>SKE14</Text></View>
 }
